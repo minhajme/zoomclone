02e111c5dcc90045e95c343ef30015788bbe29ea
MinhajulAnwar <minhaj.me.bd@gmail.com>
2023-05-13 13:35:47 +0600

    user disconnect fast emove

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
         socket.to(roomId).emit('user-connected', userId);
+        socket.on('disconnect', () => {
+            
+        });
     });
 });
 
